e48d8222aeb8322dbf6be19163df00c2ad3b781d
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2022-11-19 19:58:51 +0700

    fix: bug

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,7 @@ export default function about(){
             <div class="container px-5 py-24 mx-auto flex flex-col">
                 <div class="lg:w-4/6 mx-auto">
                     <div class="rounded-lg h-64 overflow-hidden">
-                        <Image alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500"/>
+                        <Image alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500" width="1200" height="500"/>
                     </div>
                     <div class="flex flex-col sm:flex-row mt-10">
                         <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
--- pages/components/ItemCard.js
+++ pages/components/ItemCard.js
@@ -5,7 +5,7 @@ export default function ItemCard(props) {
         return (
             <div class="lg:w-1/4 md:w-1/2 p-4 w-full">
                 <a class="block relative h-48 rounded overflow-hidden">
-                    <Image alt="thumbnail" class="object-cover object-center w-full h-full block" src={props.data.thumbnail_url} />
+                    <Image alt="thumbnail" class="object-cover object-center w-full h-full block" src={props.data.thumbnail_url} width="421" height="261" />
                 </a>
                 <div class="mt-4">
                     <h3 class="text-gray-500 text-xs tracking-widest title-font mb-1">{props.data.servings_noun_plural}</h3>
