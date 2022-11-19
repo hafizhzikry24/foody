1e34a982380b3c5d5e0cceeab402645a4165e8a6
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2022-11-19 19:31:44 +0700

    feat: update ux

---
+++ pages/components/List.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import ItemCard from './ItemCard';
+
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+        'X-RapidAPI-Host': process.env.RAPID_API_HOST
+    }
+};
+
+export default function List() {
+    const [data, setData] = useState({results: []})
+
+    useEffect(() => {
+        fetch('https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=under_30_minutes', options)
+            .then(response => response.json())
+            .then(response => setData(response))
+            .catch(err => console.error(err));
+    }, []);
+
+    return (
+        <section class="text-gray-600 body-font">
+            <div class="container px-5 py-8 mx-auto">
+                <div class="flex flex-wrap -m-4">
+                    {   
+                        data.results.length === 0 ? (<p class="mx-auto text-center font-bold">Loading...</p>) :
+                        data.results.map((item) => (
+                            <ItemCard data={item} />
+                        ))
+                    }
+                </div>
+            </div>
+        </section>
+    )
+}
