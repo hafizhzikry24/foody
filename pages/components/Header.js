95c3623b222f643f56438e4c5d5f05f3a08e0d8d
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2022-11-20 16:16:08 +0700

    fix:sticky

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,12 +3,9 @@ import Image from 'next/image'
 
 export default function Header() {
     return (
-        <header class="text-gray-600 body-font">
+        <header class="sticky top-0 text-gray-600 body-font z-50 bg-white">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link href="/" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-                    {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
-                        <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                    </svg> */}
                     <Image src="/assets/icon.png" width="40" height="40" />
                     <span class="ml-3 text-xl">Foody</span>
                 </Link>
@@ -21,5 +18,6 @@ export default function Header() {
             </div>
         </header>
 
+        
     )
 }
