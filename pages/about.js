4ba1eb4e076a3ad9e8b576ca92a6979b33154d08
hafizhzikry24 <81897441+hafizhzikry24@users.noreply.github.com>
2022-11-20 17:58:34 +0700

    feat: optimize performance

--- pages/about.js
+++ pages/about.js
@@ -1,44 +1,67 @@
-import Image from "next/image"
-import Meta from "./components/Meta"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
+import Image from "next/image";
+import Meta from "./components/Meta";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
-export default function about(){
-    return(
-        <div >
+export default function about() {
+  return (
+    <div>
       <Meta title="Foody: about" />
 
       <Header />
-      
+
       <section className="text-gray-600 body-font">
-            <div className="container px-5 py-24 mx-auto flex flex-col">
-                <div className="lg:w-4/6 mx-auto">
-                    <div className="rounded-lg h-64 overflow-hidden">
-                        <Image alt="content" className="object-cover object-center h-full w-full" src="https://www.visitsingapore.com/id_id/dining-drinks-singapore/_jcr_content/par-carousel/carousel_with_video/carousel/item1.thumbnail.carousel-img.1400.500.jpg" width="1200" height="500"/>
-                    </div>
-                    <div className="flex flex-col sm:flex-row mt-10">
-                        <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
-                            <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                                <Image src= "/assets/pasfoto.png" width ="80" height = "80"/> 
-                            </div>
-                            <div className="flex flex-col items-center text-center justify-center">
-                                <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">Muhammad Hafizh Zikry</h2>
-                                <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
-                                <p className="text-base">Just people living in the wild world and want to found the mean of life of world that created by god</p>
-                            </div>
-                        </div>
-                        <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                            <p className="leading-relaxed text-lg mb-4">Meggings portland fingerstache lyft, post-ironic fixie man bun banh mi umami everyday carry hexagon locavore direct trade art party. Locavore small batch listicle gastropub farm-to-table lumbersexual salvia messenger bag. Coloring book flannel truffaut craft beer drinking vinegar sartorial, disrupt fashion axe normcore meh butcher. Portland 90's scenester vexillologist forage post-ironic asymmetrical, chartreuse disrupt butcher paleo intelligentsia pabst before they sold out four loko. 3 wolf moon brooklyn.</p>
-                            
-                        </div>
-                    </div>
-                </div>
+        <div className="container px-5 py-24 mx-auto flex flex-col">
+          <div className="lg:w-4/6 mx-auto">
+            <div className="rounded-lg h-64 overflow-hidden">
+              <Image
+                alt="banner"
+                className="object-cover object-center h-full w-full"
+                src="https://www.visitsingapore.com/id_id/dining-drinks-singapore/_jcr_content/par-carousel/carousel_with_video/carousel/item1.thumbnail.carousel-img.1400.500.jpg"
+                width="1200"
+                height="500"
+              />
             </div>
-        </section>
+            <div className="flex flex-col sm:flex-row mt-10">
+              <div className="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+                <div className="w-20 h-20 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                  <Image
+                    alt="profile"
+                    src="/assets/pasfoto.png"
+                    width="80"
+                    height="80"
+                  />
+                </div>
+                <div className="flex flex-col items-center text-center justify-center">
+                  <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">
+                    Muhammad Hafizh Zikry
+                  </h2>
+                  <div className="w-12 h-1 bg-indigo-500 rounded mt-2 mb-4"></div>
+                  <p className="text-base">
+                    Just people living in the wild world and want to found the
+                    mean of life of world that created by god
+                  </p>
+                </div>
+              </div>
+              <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                <p className="leading-relaxed text-lg mb-4">
+                  Meggings portland fingerstache lyft, post-ironic fixie man bun
+                  banh mi umami everyday carry hexagon locavore direct trade art
+                  party. Locavore small batch listicle gastropub farm-to-table
+                  lumbersexual salvia messenger bag. Coloring book flannel
+                  truffaut craft beer drinking vinegar sartorial, disrupt
+                  fashion axe normcore meh butcher. Portland 90's scenester
+                  vexillologist forage post-ironic asymmetrical, chartreuse
+                  disrupt butcher paleo intelligentsia pabst before they sold
+                  out four loko. 3 wolf moon brooklyn.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
 
-      <Footer/>
+      <Footer />
     </div>
-  )
-    
-
+  );
 }
--- pages/components/ItemCard.js
+++ pages/components/ItemCard.js
@@ -1,20 +1,32 @@
-import Image from "next/image"
+import Image from "next/image";
 
 export default function ItemCard(props) {
-    try {
-        return (
-            <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
-                <a className="block relative h-48 rounded overflow-hidden">
-                    <Image alt="thumbnail" className="object-cover object-center w-full h-full block" src={props.data.thumbnail_url} width="421" height="261" />
-                </a>
-                <div className="mt-4">
-                    <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">{props.data.servings_noun_plural}</h3>
-                    <h2 className="text-gray-900 title-font text-lg font-medium">{props.data.name}</h2>
-                    <p className="mt-1">{props.data.num_servings} Servings</p>
-                </div>
-            </div>
-        )
-    } catch (e) {
-        console.log(e)
-    }
+  try {
+    return (
+      <div className="lg:w-1/4 md:w-1/2 p-4 w-full">
+        <a className="block relative h-48 rounded overflow-hidden">
+          <Image
+            alt="thumbnail"
+            className="object-cover object-center w-full h-full block"
+            layout="fill"
+            objectFit="cover"
+            width="421"
+            height="261"
+            src={props.data.thumbnail_url}
+          />
+        </a>
+        <div className="mt-4">
+          <h3 className="text-gray-500 text-xs tracking-widest title-font mb-1">
+            {props.data.servings_noun_plural}
+          </h3>
+          <h2 className="text-gray-900 title-font text-lg font-medium">
+            {props.data.name}
+          </h2>
+          <p className="mt-1">{props.data.num_servings} Servings</p>
+        </div>
+      </div>
+    );
+  } catch (e) {
+    console.log(e);
+  }
 }
